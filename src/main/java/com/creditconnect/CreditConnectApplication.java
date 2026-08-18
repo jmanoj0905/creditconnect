bf1c485119274fbced86898d090073d334adb59b
Manoj J <148736462+jmanoj0905@users.noreply.github.com>
2026-08-18 12:00:35 +0530

    set up spring boot project with postgres and h2 test config

---
+++ src/main/java/com/creditconnect/CreditConnectApplication.java
@@ -0,0 +1,12 @@
+package com.creditconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditConnectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/creditconnect/CreditConnectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.creditconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CreditConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
